6bc02788001e8a64bf1d1d562cd7ed84dfd4809b
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-05-09 23:31:33 +0200

    End project

--- script.js
+++ script.js
@@ -3,138 +3,142 @@ const app = new Vue({
     data: {
         contacts: [
             {
-        name: 'Michele',
-        img: 'imgs/avatar_1.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '13/08/2020 15:30:55',
-                text: 'Hai portato a spasso il cane?',
-                status: 'sent'
+                name: 'Michele',
+                img: 'imgs/avatar_1.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '13/08/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                date: '10/01/2020 15:50:00',
-                text: 'Ricordati di dargli da mangiare',
-                status: 'sent'
+                name: 'Fabio accollo',
+                img: 'imgs/avatar_2.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Stasera ceniamo tutti da me, sei invitato',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Grande!Ciao guarda mi piacerebbe moltissimo ma purtroppo ho già un impegno',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:37:55',
+                        text: 'Dai non puoi mancare',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:38:00',
+                        text: 'Ma mollami!!!',
+                        status: 'sent'
+                    }
+                ],
+            }, {
+                name: 'Samuele',
+                img: 'imgs/avatar_3.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:00',
+                        text: 'Ma che caz',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                date: '10/01/2020 16:15:22',
-                text: 'Tutto fatto!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Fabio',
-        img: 'imgs/avatar_2.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '20/03/2020 16:30:00',
-                text: 'Ciao come stai?',
-                status: 'sent'
+                name: 'Matteo pacco',
+                img: 'imgs/avatar_4.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Oh allora stasera Officine?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Non posso, ho la cena dagli suoceri',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:52:55',
+                        text: 'Sfigato',
+                        status: 'sent'
+                    },
+                ],
             },
             {
-                date: '20/03/2020 16:30:55',
-                text: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+                name: 'Mario',
+                img: 'imgs/avatar_5.jpg',
+                visible: true,
+                messages: [],
             },
             {
-                date: '20/03/2020 16:35:00',
-                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
-            }
-        ],
-    }, {
-        name: 'Samuele',
-        img: 'imgs/avatar_3.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '28/03/2020 10:10:40',
-                text: 'La Marianna va in campagna',
-                status: 'received'
+                name: 'Valentina',
+                img: 'imgs/avatar_6.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Stasera ho casa libera',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma ho mal di testa',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                date: '28/03/2020 10:20:10',
-                text: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+                name: 'Deboroh calcetto',
+                img: 'imgs/avatar_7.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Perchè ad ogni cosa che ti scrivo rispondi "ok"?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ok',
+                        status: 'received'
+                    }
+                ],
             },
-            {
-                date: '28/03/2020 16:15:22',
-                text: 'Ah scusa!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Matteo',
-        img: 'imgs/avatar_4.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Mario',
-        img: 'imgs/avatar_5.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Laura',
-        img: 'imgs/avatar_6.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Deborah',
-        img: 'imgs/avatar_7.jpg',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
-    },
         ],
         activeContact: {},
         textMessage: '',
@@ -146,7 +150,7 @@ const app = new Vue({
             return this.contacts.filter((element) => {
                 return element.name.toLowerCase().includes(this.searchContact.toLowerCase());
             });
-        }
+        },
     },
     methods: {
         onContactClick(selectedContact) {
@@ -154,27 +158,65 @@ const app = new Vue({
         },
         showMessage() {
             const newMessage = {
-                date: '13/08/2019 15:30:55',
-                // date: moment().format("DD/MM/YYYY HH:mm:ss"),
+                date: moment().format("DD/MM/YYYY HH:mm:ss"),
                 text: this.textMessage,
                 status: 'sent'
             }
-            
+
             this.activeContact.messages.push(newMessage);
             this.textMessage = '';
 
             setTimeout(() => {
                 const defaultAnswer = {
-                    date: '13/08/2019 15:30:55',
+                    date: moment().format("DD/MM/YYYY HH:mm:ss"),
                     text: 'Ok',
                     status: 'received'
                 }
                 this.activeContact.messages.push(defaultAnswer)
-            }, 1000)
+            }, 3000)
         },
         onInput() {
             const filteredContacts = this.contacts.filter((element) => element.name === this.searchContact);
             console.log(filteredContacts)
+        },
+        formatTime(stringDate) {
+            return moment(stringDate, "DD/MM/YYYY hh:mm:ss").format("HH:mm")
+        },
+        onMessageClick(message, event) {
+            this.$set(message, "showPopUp", true);
+
+            event.currentTarget.focus()
+        },
+        onMouseOut(message) {
+            this.$set(message, "showPopUp", false);
+        },
+        onInfoClick(message) {
+            message.showPopUp = false;
+        },
+        onDeleteClick(messageIndex) {
+            this.activeContact.messages.splice(messageIndex, 1)
+        },
+        getLastMessage(messages) {
+            if (messages.length === 0) {
+                return "Questa conversazione non contiene nessun messaggio"
+            }
+
+            const lastMessage = messages[messages.length - 1];
+
+            const slicedMessage = lastMessage.text.slice(0, 20) + "..."
+
+            return slicedMessage
+        },
+        getLastMessageTime(messages) {
+            if (messages.length === 0) {
+                return " "
+            }
+
+            const lastMsg = messages[messages.length - 1];
+
+            const lastMessageTime = this.formatTime(lastMsg.date)
+
+            return lastMessageTime
         }
     }
 })
